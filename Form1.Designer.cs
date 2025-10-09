9a6e18a358427823519c11f02bda06d774e164dd
AdranLTM <adranltm@seznam.cz>
2025-10-09 13:05:44 +0200

    nigga

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,29 +30,65 @@
         {
             textBox1 = new TextBox();
             button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(157, 52);
+            textBox1.Location = new Point(149, 76);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(487, 27);
+            textBox1.Size = new Size(514, 27);
             textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(238, 152);
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button1.Location = new Point(243, 135);
             button1.Name = "button1";
             button1.Size = new Size(321, 41);
             button1.TabIndex = 1;
             button1.Text = "Calculate";
             button1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 238);
+            label1.Location = new Point(369, 235);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Osa čísel";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label2.Location = new Point(352, 21);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Gingerlator";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(369, 190);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 20);
+            label3.TabIndex = 4;
+            label3.Text = "vysledek:";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(textBox1);
             Name = "Form1";
@@ -65,5 +101,8 @@
 
         private TextBox textBox1;
         private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
